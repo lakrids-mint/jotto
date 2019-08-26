d927bc183b2c5a161cf67b2f0228e292c2259fe8
lakrids-mint <mettepernille@hotmail.com>
2019-08-26 23:44:55 +0200

    add action creator and reducer for guessed word

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,6 @@
 export function getLetterMatchCount(guessedWord, secretWord) {
-  const secretLetterSet = new Set(secretWord.split(""));
   const guessedLetterSet = new Set(guessedWord.split(""));
+  const secretLetterSet = new Set(secretWord.split(""));
   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter))
     .length;
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import success from "./succesReducer";
 import guessedWords from "./guessedWordReducer";
+import secretWord from "./secretwordReducer";
 
 export default combineReducers({
   success,
-  guessedWords
+  guessedWords,
+  secretWord
 });
